960fb590fa0709d961ab0a9eb1db40ef1950853b
Ayanaksha Upadhyaya <172816128+uayanaksha@users.noreply.github.com>
2025-07-17 23:10:40 +0530

    !accept https://codeforces.com/contest/4/submission/329530240

--- 4/a/main.c
+++ 4/a/main.c
@@ -2,9 +2,15 @@
 int main(){
   int n;
   scanf("%d", &n);
-  if (n%2) {
+  if (n<4) {
+    printf("NO");
+    return 0;
+  }
+    
+  if(n%2) {
     printf("NO");
   } else {
     printf("YES");
   }
+  return 0;
 }
