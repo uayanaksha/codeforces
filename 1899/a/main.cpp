aa957924d2045525d4144b19d06715079be09504
Ayanaksha Upadhyaya <172816128+uayanaksha@users.noreply.github.com>
2025-07-26 02:14:48 +0530

    !accept https://codeforces.com/problemset/submission/1899/330872858

---
+++ 1899/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int t, n;
+  cin>>t;
+  while(t--){
+    cin>>n;
+    if(n%3 == 0){
+      println("Second");
+    } else {
+      println("First");
+    }
+  }
+  return 0;
+}
