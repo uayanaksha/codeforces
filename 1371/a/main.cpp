7b62c1ddca4101582af69c553aa04bdf03d4e42a
Ayanaksha Upadhyaya <172816128+uayanaksha@users.noreply.github.com>
2025-07-30 04:13:39 +0530

    !accept https://codeforces.com/problemset/submission/1371/331516082

---
+++ 1371/a/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    if(n < 3){
+      cout << 1 << endl;
+    } else {
+      cout << (n/2) + (n%2) << endl;
+    }
+  }
+  return 0;
+}
