414f92cbe2526f55614407ecd109cb5f0cf9a2a5
Ayanaksha Upadhyaya <172816128+uayanaksha@users.noreply.github.com>
2025-07-29 08:06:55 +0530

    !accept https://codeforces.com/problemset/submission/2044/331373908

---
+++ 2044/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    cout << (2*n - 2)/2 << endl;
+  }
+  return 0;
+}
