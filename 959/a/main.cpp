ae2760d47b4c65e2f3f4f41bdd9d17a59da1eaa2
Ayanaksha Upadhyaya <172816128+uayanaksha@users.noreply.github.com>
2025-07-29 07:36:41 +0530

    !accept https://codeforces.com/problemset/submission/959/331371335

---
+++ 959/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int n;
+  cin >> n;
+  if(n%2){
+    cout << "Ehab" << endl;
+  } else {
+    cout << "Mahmoud" << endl;
+  }
+  return 0;
+}
