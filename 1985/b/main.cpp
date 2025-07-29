9dc175f2281644924b18832bf4c8feeb3c510b2a
Ayanaksha Upadhyaya <172816128+uayanaksha@users.noreply.github.com>
2025-07-29 06:31:34 +0530

    !accept https://codeforces.com/problemset/submission/1985/331366159
    note: guessed impl. logic unclear

---
+++ 1985/b/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    if(n < 4){
+      cout << n << endl;
+    } else {
+      cout << 2 << endl;
+    }
+  }
+  return 0;
+}
