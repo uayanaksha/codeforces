834d477f503d0573ef9f4f9a4251b6f010b7e17f
Ayanaksha Upadhyaya <172816128+uayanaksha@users.noreply.github.com>
2025-07-31 17:21:41 +0530

    !accept https://codeforces.com/problemset/submission/233/331727154

---
+++ 233/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int n;
+  cin >> n;
+  if(n % 2 || n < 2){
+    cout << -1 << endl;
+  } else {
+    for(int i{2}; i<=n; i+=2){
+      cout << i << " " << i-1 << " ";
+    } cout << endl;
+  }
+  return 0;
+}
