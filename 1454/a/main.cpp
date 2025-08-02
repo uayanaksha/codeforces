423d4af7a3bbc9bee15e713edfcbe916c78e75bf
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-08-03 02:41:23 +0530

    !accept https://codeforces.com/problemset/submission/1454/332153644

---
+++ 1454/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    for(int i=1; i<n; i++){
+      cout << i+1 << " ";
+    } cout << 1 << endl;
+  }
+  return 0;
+}
