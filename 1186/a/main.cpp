9b4cace965118387ca5916abb07122b146cad0ba
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-08-06 07:47:32 +0530

    !accept https://codeforces.com/problemset/submission/1186/332576446

---
+++ 1186/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int n, m, k;
+  cin >> n >> m >> k;
+  if(n>m || n>k){
+    cout << "NO" << endl;
+  } else {
+    cout << "YES" << endl;
+  }
+  return 0;
+}
