4a993d993e281ed3b751054b52f2e3777cb9591f
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-08-09 10:21:54 +0530

    !accept https://codeforces.com/problemset/submission/2033/333085553

---
+++ 2033/a/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    if(n%2){
+      cout << "Kosuke" << endl;
+    } else {
+      cout << "Sakurako" << endl;
+    }
+  }
+  return 0;
+}
