ffbeac303f8dc23433e26baddc1ea7d1e87e5d89
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-08-26 19:32:22 +0530

    !accept https://codeforces.com/problemset/submission/1549/335613590

---
+++ 1549/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+  int t;
+  cin >> t;
+  while(t--){
+    int n, a, b;
+    cin >> n;
+    a = n/2;
+    b = a*2;
+    cout << a << " " << b << endl;
+  }
+  return 0;
+}
