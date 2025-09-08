d37b511879af7393d3ba1a5a186dbd109406db8a
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-09-08 06:05:39 +0530

    !accept https://codeforces.com/problemset/submission/1372/337481874

---
+++ 1372/a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(void){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    while(n--){
+      cout << 1 << " ";
+    } cout << endl;
+  }
+  return 0;
+}
