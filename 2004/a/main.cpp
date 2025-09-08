0d3e904994dc4116f7647ac85fa4653491e5243d
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2025-09-08 06:42:22 +0530

    !accept https://codeforces.com/problemset/submission/2004/337483337

---
+++ 2004/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(void){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    vector<ll> mem(n);
+    for(ll &i: mem){
+      cin >> i;
+    }
+    bool signal = (n == 2 && abs(mem.front() - mem.back()) > 1);
+    cout << ( signal ? "YES" : "NO" ) << endl;
+  }
+  return 0;
+}
