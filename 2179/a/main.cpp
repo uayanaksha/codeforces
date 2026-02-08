79a48840a0b24c0edac0dacd45d16e877c9664ae
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-02-08 18:58:58 +0530

    !accept https://codeforces.com/problemset/submission/2179/362010315
    note: Brute forced from the output pattern. Didn't understand shit

---
+++ 2179/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(void){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while(t--){
+    int k, x;
+    cin >> k >> x;
+    cout << k*x + 1 << endl;
+ }
+  return 0;
+}
