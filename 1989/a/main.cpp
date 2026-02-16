454175e81dd495887e5a36209685295b02ae0f72
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-02-17 04:38:11 +0530

    !accept https://codeforces.com/problemset/submission/1989/363343952

---
+++ 1989/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(void){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while(t--){
+    int x, y;
+    cin >> x >> y;
+    cout << (y >= -1 ? "YES" : "NO") << endl;
+  }
+  return 0;
+}
